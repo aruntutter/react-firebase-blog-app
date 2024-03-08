73aa140bec437190110668b47fb57f26fc190327
Arun Kumar R <aruntutter19@gmail.com>
2024-03-08 23:51:20 +0530

    created footer and styled

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./Footer.css";
+import Logo from "../../assets/logo.png";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa6";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer-logo">
+        <img src={Logo} alt="logo" />
+      </div>
+      <p>&copy; 2024 BlogOps</p>
+      <div class="social-icons">
+        <ul>
+          <li>
+            <FaXTwitter />
+          </li>
+          <li>
+            <FaGithub />
+          </li>
+          <li>
+            <FaLinkedinIn />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
