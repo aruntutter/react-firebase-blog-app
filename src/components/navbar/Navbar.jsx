210acacbf6dfa3804fbf2bd920506701196c157d
Arun Kumar R <aruntutter19@gmail.com>
2024-03-08 23:51:01 +0530

    created navbar and styled

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+import UserIcon from "../../assets/user-icon.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div className="navbar">
+      {/* Logo */}
+      <div className="logo">
+        <Link to="/">
+          <img src={Logo} alt="logo" />
+        </Link>
+      </div>
+      {/* Hamburger Menu */}
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      {/* Links */}
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/allblogs"}>Blogs</Link>
+        </li>
+        <li>
+          <Link to={"/adminlogin"}>Login</Link>
+        </li>
+        <li>
+          <Link to={"/dashboard"}>
+            <div className="profile-pic">
+              <img src={UserIcon} alt="" />
+            </div>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
