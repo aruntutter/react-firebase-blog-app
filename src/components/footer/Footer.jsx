ed10004c59d5465b5dbc2da6d3f853cea6957832
Arun Kumar R <aruntutter19@gmail.com>
2024-03-09 09:51:03 +0530

    footer css updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,25 +7,27 @@ import { FaLinkedinIn } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <div className="footer-logo">
-        <img src={Logo} alt="logo" />
+    <footer>
+      <div className="footer">
+        <div className="footer-logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <p>&copy; 2024 BlogOps</p>
+        <div class="social-icons">
+          <ul>
+            <li>
+              <FaXTwitter />
+            </li>
+            <li>
+              <FaGithub />
+            </li>
+            <li>
+              <FaLinkedinIn />
+            </li>
+          </ul>
+        </div>
       </div>
-      <p>&copy; 2024 BlogOps</p>
-      <div class="social-icons">
-        <ul>
-          <li>
-            <FaXTwitter />
-          </li>
-          <li>
-            <FaGithub />
-          </li>
-          <li>
-            <FaLinkedinIn />
-          </li>
-        </ul>
-      </div>
-    </div>
+    </footer>
   );
 };
 
