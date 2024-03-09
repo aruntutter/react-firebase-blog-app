7fae415bda04ffc747d90df8fd2b411bf070ef94
Arun Kumar R <aruntutter19@gmail.com>
2024-03-09 10:44:26 +0530

    loader created and styled

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./Loader.css";
+import Spinner from "../../assets/loader-spinner.gif";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="loader">
+      <div className="loader-icon">
+        <img src={Spinner} alt="Spinner Loader" />
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
