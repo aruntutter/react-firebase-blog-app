7bb33deae71cfcd7b589117f035dc46ee1a4f785
Arun Kumar R <aruntutter19@gmail.com>
2024-03-09 10:17:12 +0530

    Displayed: BlogPostCard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Layout from "../../components/layout/Layout";
+import BlogPostCard from "../../components/blogPostCard/BlogPostCard";
 
 const Home = () => {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <BlogPostCard />
+    </Layout>
+  );
 };
 
 export default Home;
