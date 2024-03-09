833fa5ff6245218a821f442548bb9eb6f1cc7d76
Arun Kumar R <aruntutter19@gmail.com>
2024-03-09 10:32:23 +0530

    deleted search option

--- src/components/searchDialog/SearchDialog.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./SearchDialog.css";
-
-const SearchDialog = () => {
-  return <div>SearchDialog</div>;
-};
-
-export default SearchDialog;
--- src/components/shareDialogBox/ShareDialogBox.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ShareDialogBox.css";
-
-const ShareDialogBox = () => {
-  return <div>ShareDialogBox</div>;
-};
-
-export default ShareDialogBox;
