f2a7af181fb21444363178e3b381f45bcc82c7fc
Arun Kumar R <aruntutter19@gmail.com>
2024-03-09 11:30:21 +0530

    login form created and styled

--- src/pages/admin/adminLogin/AdminLogin.jsx
+++ src/pages/admin/adminLogin/AdminLogin.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./AdminLogin.css";
 
 const AdminLogin = () => {
-  return <div>AdminLogin</div>;
+  return (
+    <div className="admin-login-container">
+      <div className="admin-login-form">
+        <h2>Login</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <div className="submit-btn">
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLogin;
