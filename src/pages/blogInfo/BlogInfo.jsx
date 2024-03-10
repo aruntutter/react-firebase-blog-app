ea6ba6ce664bfbae8dc1fffd803bef0095c6e797
Arun Kumar R <aruntutter19@gmail.com>
2024-03-10 14:24:09 +0530

    displayed blog in BlogInfo

--- src/pages/blogInfo/BlogInfo.jsx
+++ src/pages/blogInfo/BlogInfo.jsx
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import "./BlogInfo.css";
+import Navbar from "../../components/navbar/Navbar";
+import Logo from "../../assets/logo.png";
+import { myContext } from "../../context/data/myContext";
+import { doc, getDoc } from "firebase/firestore";
+import { fireDb } from "../../firebase/FirebaseConfig";
+import { useParams } from "react-router-dom";
+import Loader from "../../components/loader/Loader";
 
 const BlogInfo = () => {
-  return <div>BlogInfo</div>;
+  const context = useContext(myContext);
+  const { loading, setLoading } = context;
+
+  const params = useParams();
+
+  //* getBlogs State
+  const [getBlog, setGetBlog] = useState();
+
+  const getAllBlogs = async () => {
+    setLoading(true);
+    try {
+      const productRef = doc(fireDb, "blogPost", params.id);
+      const productTemp = await getDoc(productRef);
+      if (productTemp.exists()) {
+        setGetBlog(productTemp.data());
+      } else {
+        console.log("Document does not exist");
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getAllBlogs();
+  }, [params.id]);
+
+  console.log(getBlog);
+
+  return (
+    <>
+      <Navbar />
+      <div className="blog-info-container">
+        <div className="blog-info-container">
+          {loading ? (
+            <Loader />
+          ) : getBlog ? (
+            <div className="custom-card">
+              {/* Thumbnail */}
+              <div className="custom-post-thumbnail">
+                <img src={getBlog.thumbnail} alt={getBlog.title} />
+              </div>
+
+              {/* Details */}
+              <div className="custom-post-details">
+                <div className="post-detail-text">
+                  {/* Title */}
+                  <h3>{getBlog.title}</h3>
+                  {/* Description */}
+                  <p>{getBlog.description}</p>
+                </div>
+
+                {/* Date and Logo */}
+                <div className="date-logo">
+                  {/* Date */}
+                  <p>{getBlog.date}</p>
+                  {/* Logo */}
+                  <div className="custom-logo">
+                    <img src={Logo} alt="Logo" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <p>Blog not found</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BlogInfo;
