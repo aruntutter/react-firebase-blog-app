18c0f023d72f85eda8528a07cf00d4efb5accc2e
Arun Kumar R <aruntutter19@gmail.com>
2024-03-10 11:10:36 +0530

    blogs fetched from firebase and displayed in home

--- src/components/blogPostCard/BlogPostCard.jsx
+++ src/components/blogPostCard/BlogPostCard.jsx
@@ -1,81 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./BlogPostCard.css";
+import { myContext } from "../../context/data/myContext";
+import { useNavigate } from "react-router-dom";
 
 const BlogPostCard = () => {
+  const context = useContext(myContext);
+  const { loading, getAllBlog } = context;
+  // console.log(getAllBlog);
+
+  const navigate = useNavigate();
+
   return (
     <div className="blog-post-card">
       <div className="post-cards">
-        {/* Card 1 */}
-        <div className="card">
-          {/* Thumbnail */}
-          <div className="post-thumbnail">
-            <img
-              src="https://www.invajy.com/wp-content/uploads/2022/11/Into-the-Wild-Quotes-3.jpeg"
-              alt="Into the wild movie - quote"
-            />
-          </div>
+        {/* Card */}
+        {getAllBlog && getAllBlog.length > 0 ? (
+          <>
+            {getAllBlog.map((item, index) => {
+              console.log(item);
+              const { thumbnail, date, title, description, id } = item;
+              return (
+                <div className="card" key={index}>
+                  {/* Thumbnail */}
+                  <div className="post-thumbnail">
+                    <img src={thumbnail} alt={"Blog - Image"} />
+                  </div>
 
-          {/* Details */}
-          <div className="post-details">
-            {/* Date */}
-            <p>09-03-2024</p>
-            {/* Title */}
-            <h3>Into The Wild</h3>
-            {/* Description */}
-            <p>
-              Christopher McCandless: When you want something in life, you just
-              gotta reach out and grab it.
-            </p>
-          </div>
-        </div>
-
-        {/* Card 2 */}
-        <div className="card">
-          {/* Thumbnail */}
-          <div className="post-thumbnail">
-            <img
-              src="https://www.invajy.com/wp-content/uploads/2022/11/Into-the-Wild-Quotes-1-600x441.jpeg"
-              alt="Into the wild movie - quote"
-            />
-          </div>
-
-          {/* Details */}
-          <div className="post-details">
-            {/* Date */}
-            <p>04-03-2024</p>
-            {/* Title */}
-            <h3>Christopher McCandless</h3>
-            {/* Description */}
-            <p>
-              “People just need to change the way they look at things.” ~
-              Christopher McCandless, “Into The Wild”
-            </p>
-          </div>
-        </div>
-
-        {/* Card 3 */}
-        <div className="card">
-          {/* Thumbnail */}
-          <div className="post-thumbnail">
-            <img
-              src="https://www.invajy.com/wp-content/uploads/2022/11/Into-the-Wild-Quotes-2-600x397.jpeg"
-              alt="Into the wild movie - quote"
-            />
-          </div>
-
-          {/* Details */}
-          <div className="post-details">
-            {/* Date */}
-            <p>08-03-2024</p>
-            {/* Title */}
-            <h3>Christopher McCandless</h3>
-            {/* Description */}
-            <p>
-              “Happiness is only real when shared.” ~ Christopher McCandless,
-              “Into The Wild”
-            </p>
-          </div>
-        </div>
+                  {/* Details */}
+                  <div
+                    className="post-details"
+                    onClick={() => {
+                      navigate(`/bloginfo/:id${id}`);
+                    }}
+                  >
+                    {/* Date */}
+                    <p>{date}</p>
+                    {/* Title */}
+                    <h3>{title}</h3>
+                    {/* Description */}
+                    <p>{description}</p>
+                  </div>
+                </div>
+              );
+            })}{" "}
+          </>
+        ) : (
+          <h1>Not Found🙁</h1>
+        )}
       </div>
       {/* See More Button */}
       <div className="see-more-btn">
