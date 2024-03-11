bfde6eda9074c41789624f09f1a2592a296348f4
Arun Kumar R <aruntutter19@gmail.com>
2024-03-11 15:45:59 +0530

    removed component: Blog

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import {
   Navigate,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Blog from "./pages/blog/Blog";
 import AllBlogs from "./pages/allBlogs/AllBlogs";
 import NoPage from "./pages/nopage/Nopage";
 import BlogInfo from "./pages/blogInfo/BlogInfo";
@@ -21,7 +20,6 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
           <Route path="/allblogs" element={<AllBlogs />} />
           <Route path="/bloginfo/:id" element={<BlogInfo />} />
           <Route path="/adminlogin" element={<AdminLogin />} />
